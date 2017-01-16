53895fceaa328b03d4f35499b12e67469daeef8a
guohao <guohao0817@gmail.com>
2017-01-16 22:51:51 +0800

    update

--- calcproj/simplemath/sqrt_test.go
+++ calcproj/simplemath/sqrt_test.go
@@ -4,7 +4,7 @@ package simplemath
 import "testing"
 
 func TestSqrt1(t *testing.T) {
-  v := Sqrt(16)
+  v := Sqrt(1677)
   if v != 4 {
     t.Errorf("Sqrt(16) failed. Got %v, expected 4.", v)
   }
