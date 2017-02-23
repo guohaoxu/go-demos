b5ebb7bb0f60d4cf60fd81dff28e50edce9f9f3b
guohao <guohao0817@gmail.com>
2017-02-23 11:00:09 +0800

    update

--- db_mysql/mysql.go
+++ db_mysql/mysql.go
@@ -7,16 +7,49 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/world")
+func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test")
+	checkErr(err)
 	defer db.Close()
 
-	err = db.Ping()
-	if err != nil {
-		panic(err)
+	// 更新数据
+	stmt, err := db.Prepare("INSERT userinfo SET username=?, departname=?, created=?")
+	checkErr(err)
+	res, err := stmt.Exec("guohao", "研发部门", "2017-02-23")
+	checkErr(err)
+	id, err := res.LastInsertId()
+	checkErr(err)
+	fmt.Println(id)
+
+	// 查询数据
+	rows, err := db.Query("SELECT * FROM userinfo")
+	checkErr(err)
+	for rows.Next() {
+		var uid int
+		var username string
+		var department string
+		var created string
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println(uid)
+		fmt.Println(username)
+		fmt.Println(department)
+		fmt.Println(created)
 	}
-	fmt.Println("mysql has connected")
+
+	// 删除数据
+	stmtDel, err := db.Prepare("DELETE FROM userinfo WHERE uid=?")
+	checkErr(err)
+	resDel, err := stmtDel.Exec(2)
+	checkErr(err)
+	affect, err := resDel.RowsAffected()
+	checkErr(err)
+	fmt.Println(affect)
+
 }
